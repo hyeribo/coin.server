39405a831c3d4b16e05ae783db5b2e1316eb8137
유혜리 <yuhyeri@yuhyeliui-MacBookPro.local>
2022-01-05 14:05:56 +0900

    Remove AccountService.getMyCoinInfo

--- src/services/AccountService.ts
+++ src/services/AccountService.ts
@@ -5,7 +5,6 @@ import { MyCoinModel } from '@src/models/MyCoin';
 
 export interface AccountServiceModel {
   getAccountInfo: () => Promise<object[]>;
-  getMyCoinInfo: (market: string) => Promise<object[]>;
 }
 
 export default class AccountService implements AccountServiceModel {
@@ -33,34 +32,6 @@ export default class AccountService implements AccountServiceModel {
         main: 'AccountService',
         sub: 'getAccountInfo',
         data: error,
-      });
-      throw error;
-    }
-  }
-
-  /**
-   * 내가 보유한 종목의 주문 가능 정보
-   * @param market 조회할 종목코드
-   * @returns 주문 가능 정보
-   */
-  async getMyCoinInfo(market: string): Promise<object[]> {
-    try {
-      const res = await privateAPI.get('/orders/chance', {
-        params: { market: market },
-      });
-
-      logger.info('data.', {
-        main: 'AccountService',
-        sub: 'getCoinOrderInfo',
-        data: res.data,
-      });
-
-      return res.data || [];
-    } catch (error) {
-      logger.error('error.', {
-        main: 'AccountService',
-        sub: 'getCoinOrderInfo',
-        data: { error },
       });
       throw error;
     }
